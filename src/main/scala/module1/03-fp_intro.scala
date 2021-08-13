ed1ec7f52f25fa805924724e86d1c7d7ce3892e3
glebka191 <gantosik@tsconsulting.com>
2021-08-14 00:36:29 +0300

    List

--- src/main/scala/module1/03-fp_intro.scala
+++ src/main/scala/module1/03-fp_intro.scala
@@ -1,138 +1,6 @@
 package module1
 
-import java.time.Instant
-import java.util.UUID
-
-import module1.List.List.Cons
-
-import scala.annotation.tailrec
-
-
-/**
- * referential transparency
- */
-object referential_transparency {
-
-
-  case class Abiturient(id: String, email: String, fio: String)
-
-  type Html = String
-
-  sealed trait Notification
-
-  object Notification {
-
-    case class Email(email: String, text: Html) extends Notification
-
-    case class Sms(telephone: String, msg: String) extends Notification
-
-  }
-
-
-  case class AbiturientDTO(email: String, fio: String, password: String)
-
-  trait NotificationService {
-    def sendNotification(notification: Notification): Unit
-
-    def createNotification(abiturient: Abiturient): Notification
-  }
-
-  trait AbiturientService {
-
-    def registerAbiturient(abiturientDTO: AbiturientDTO): Abiturient
-  }
-
-  class AbiturientServiceImpl(notificationService: NotificationService) extends AbiturientService {
-
-    override def registerAbiturient(abiturientDTO: AbiturientDTO): Abiturient = {
-      val abiturient = Abiturient(UUID.randomUUID().toString(), abiturientDTO.email, abiturientDTO.fio)
-      notificationService.sendNotification(Notification.Email(abiturient.email, "Some message"))
-      abiturient
-    }
-
-    def registerAbiturient2(uuid: UUID, abiturientDTO: AbiturientDTO): Abiturient = {
-      val abiturient = Abiturient(uuid.toString(), abiturientDTO.email, abiturientDTO.fio)
-      abiturient
-    }
-
-  }
-
-}
-
-
-// recursion
-
-object recursion {
-
-  /**
-   * Реализовать метод вычисления n!
-   * n! = 1 * 2 * ... n
-   */
-
-  def fact(n: Int): Int = {
-    var _n = 1
-    var i = 2
-    while (i <= n) {
-      _n *= i
-      i += 1
-    }
-    _n
-  }
-
-  def factRec(n: Int): Int =
-    if (n == 1) 1
-    else n * factRec(n - 1)
-
-  def factTailRec(n: Int): Int = {
-    @tailrec
-    def loop(n: Int, accum: Int): Int =
-      if (n == 1) accum
-      else loop(n - 1, n * accum)
-
-    loop(n, 1)
-  }
-
-
-  /**
-   * реализовать вычисление N числа Фибоначчи
-   * F0 = 0, F1 = 1, Fn = Fn-1 + Fn - 2
-   *
-   */
-
-  def fib(n: Int): Int = ???
-
-}
-
-object hof {
-
-  def printFactorialResult(r: Int) = println(s"Factorial result is ${r}")
-
-  def printFibonacciResult(r: Int) = println(s"Fibonacci result is ${r}")
-
-  def printResult[T](r: T, funcName: String) = println(s"$funcName result is ${r}")
-
-  def printRunningTimeFunc1[A, B](a: A)(f: A => B): Unit = {
-    val current = Instant.now().toEpochMilli()
-    f(a)
-    val current2 = Instant.now().toEpochMilli()
-    println(current2 - current)
-  }
-
-
-  // Follow type implementation
-  def partial[A, B, C](a: A, f: (A, B) => C): B => C = b => f(a, b)
-
-  def sum(x: Int, y: Int): Int = x + y
-
-  val r: Int => Int = partial(1, sum)
-
-}
-
-
-/**
- * Реализуем тип Option
- */
-
+import module1.list.List.{Cons, Nil}
 
 object opt {
 
@@ -163,7 +31,6 @@ object opt {
     }
 
     /**
-     *
      * Реализовать метод printIfAny, который будет печатать значение, если оно есть
      */
 
@@ -176,7 +43,6 @@ object opt {
      *
      * Реализовать метод zip, который будет создавать Option от пары значений из 2-х Option
      */
-
     def zip[B](that: Option[B]): Option[(T, B)] = this match {
       case Option.None => Option.None
       case Option.Some(v) => that match {
@@ -221,67 +87,58 @@ if (isEmpty || that.isEmpty) None else Some((this.get, that.get))
  * Cons - непустой, содердит первый элемент (голову) и хвост (оставшийся список)
  */
 
-object List {
+object list {
 
   sealed trait List[+T] {
-    def cons[TT >: T](head: TT): List[TT] = Cons(head, this)
-
-
-    /**
-     * Метод cons, добавляет элемент в голову списка, для этого метода можно воспользоваться названием `::`
-     *
-     */
     //  def :: [B >: A](elem: B): List[B] =  new ::(elem, this)
-      def cons[T](head: T, tail: List[T]): List[T] = Cons(head, tail)
+    def ::[B >: T](elem: B): List[B] = Cons(elem, this)
 
-    /**
-     * Метод mkString возвращает строковое представление списка, с учетом переданного разделителя
-     *
-     */
-    
+    def apply[T](arg: T*): List[T] = {
+      var l: List[T] = List.Nil
+      arg.foreach(el => l = el :: l)
+      l
+    }
 
-    /**
-     * Конструктор, позволяющий создать список из N - го числа аргументов
-     * Для этого можно воспользоваться *
-     *
-     * Например вот этот метод принимает некую последовательность аргументов с типом Int и выводит их на печать
-     * def printArgs(args: Int*) = args.foreach(println(_))
-     */
+    def mkString(str: String = ","): String = this match {
+      case List.Cons(head, tail) => s"$head$str" ++ tail.mkString(str)
+      case List.Cons(head, Nil) => s"$head"
+      case List.Nil => ""
+    }
 
-    /**
-     *
-     * Реализовать метод reverse который позволит заменить порядок элементов в списке на противоположный
-     */
+    def reverse(): List[T] = {
+      import list.List
 
-    /**
-     *
-     * Реализовать метод map для списка который будет применять некую ф-цию к элементам данного списка
-     */
+      def loop(l: List[T], acc: List[T]): List[T] =
+        l match {
+          case List.Nil => acc
+          case head :: tail => loop(tail, head :: acc)
+        }
 
+      loop(this, Nil)
+    }
 
-    /**
-     *
-     * Реализовать метод filter для списка который будет фильтровать список по некому условию
-     */
+    def map[T](f: T => T): List[T] = this match {
+      case List.Nil => List.Nil
+      case List.Cons(h, t) => f(h) :: t.map(f)
+    }
 
-    /**
-     *
-     * Написать функцию incList котрая будет принимать список Int и возвращать список,
-     * где каждый элемент будет увеличен на 1
-     */
+    def filter(f: T => Boolean): List[T] = this match {
+      case List.Cons(head, tail) => if (f(head)) head :: tail.filter(f) else tail.filter(f)
+      case List.Nil => List.Nil
+    }
 
+    def incList(list: List[Int]): List[Int] = list.map(_ + 1)
 
-    /**
-     *
-     * Написать функцию shoutString котрая будет принимать список String и возвращать список,
-     * где к каждому элементу будет добавлен префикс в виде '!'
-     */
+    def shoutString(list: List[String]): List[String] = list.map(_ + "!")
+
   }
 
   object List {
 
-    case object Nil extends List[Nothing]
     case class Cons[T](head: T, tail: List[T]) extends List[T]
-  }
-}
 
+    case object Nil extends List[Nothing]
+
+  }
+
+}
